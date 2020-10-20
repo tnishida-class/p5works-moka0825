24edca868e6c605933f1986bb6482450630e183e
1910638h <65203057+moka0825@users.noreply.github.com>
2020-10-20 13:06:22 +0900

    section2-q1 change
    途中からクロームでレビューが読み込めなくなってしまい、完成したかどうか確認できませんでした。

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,15 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  createCanvas(300,300);
+  for(let i = 0; i < 10; i = i++){
+  if(i < 5){
+    stroke(255,0,0)
+  }
+  else{
+    stroke(0,0,255)
+  }
+  ellipse(150,150,250-25*i);
+
+   // BLANK[1]
   }
 }
