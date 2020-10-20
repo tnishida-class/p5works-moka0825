1fd97dec9481f7be4f972ca02c402b5944a46a2c
1910638h <65203057+moka0825@users.noreply.github.com>
2020-10-20 23:25:14 +0900

    section2-q2 change

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,16 +5,34 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-          if(i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1){
-        fill(255);
-
-      }
+      if(i % 2 == 1){
+        if(j % 2 == 1){
+          fill(255);
+          }
+        else{
+          fill(196);
+        }
+    }
+      if(i % 2 == 0){
+        if(j % 2 == 1){
+          fill(196);
+          }
           else{
-        fill(196);
-      }
+            fill(255);
+          }
+    }
 
       console.log(i, j);
-       rect(10 * i, 10 * j, 10, 10);
+      rect(size * i, size * j, size, size);
+
+      for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 8; j ++){
+          console.log(i, j);
+          ellipse(2*size*i+b, size*j+12.5);
+          b=(j % 2 == 1 ? 12.5 : 12.5+25)
+          fill(255, 0, 0)
+        }
+      }
 
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
