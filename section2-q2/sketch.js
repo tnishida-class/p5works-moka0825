581c39f02fb7e233045b834360c2cd262e2d9538
1910638h <65203057+moka0825@users.noreply.github.com>
2020-10-20 13:07:10 +0900

    section2-q2 change
    格子模様のところまでしかわかりませんでした

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,6 +5,17 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+          if(i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1){
+        fill(255);
+
+      }
+          else{
+        fill(196);
+      }
+
+      console.log(i, j);
+       rect(10 * i, 10 * j, 10, 10);
+
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }
