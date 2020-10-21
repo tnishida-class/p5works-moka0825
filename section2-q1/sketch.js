ace77b03bce35f86da4189234ea552a30365a85b
1910638h <65203057+moka0825@users.noreply.github.com>
2020-10-21 09:09:55 +0900

    section2-q1 change
    chromeで開こうとすると、読み込みに時間がかかって、エラーになるので完成形を確認できてないです。

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,6 @@
 // 小手調べ
 function setup(){
-  createCanvas(300,300);
+  createCanvas(200,200);
   for(let i = 0; i < 10; i = i++){
   if(i < 5){
     stroke(255,0,0)
@@ -8,7 +8,7 @@ function setup(){
   else{
     stroke(0,0,255)
   }
-  ellipse(150,150,250-25*i);
+  ellipse(100,100,200-20*i);
 
    // BLANK[1]
   }
