8c296c8cce3506f9541afd9efa72aa6b601a5b59
1910638h <65203057+moka0825@users.noreply.github.com>
2020-10-27 12:42:33 +0900

    section3-q1 change

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,31 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  regularPolygon(3, 130, 16, 20);
+  balloon("I love keyakizaka46", 30, 40);
+
 }
 
-function balloon(t){
+
+function balloon(t, x, y){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  rect(0, 0, w + p * 10, h + p * 10);
   fill(255);
   text(t, p, h + p);
 }
+
+function regularPolygon(n, cx, cy, r){
+  beginShape();
+  for(var i = 0; i < 5; i++){
+    let theta = TWO_PI * i / n - HALF_PI;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    fill(0);
+    vertex(x,y);
+  }
+
+  endShape(CLOSE);
+  }
