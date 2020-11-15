12d8602db7228c2b7f4de5ba67db5f309296e885
1910638h <65203057+moka0825@users.noreply.github.com>
2020-11-15 22:29:32 +0900

    section4-q2 change
    change

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -7,13 +7,17 @@ function setup(){
   y = height / 2;
   vx = 8;
   vy = 8;
+  angle = 0;
 }
 
 function draw(){
   background(160, 192, 255);
-  // BLANK[2] (hint: 作った star 関数を使います)
+  star(x, y, 45, angle);
+  angle += 0.05;  // BLANK[2] (hint: 作った star 関数を使います)
 
   // 端の処理パターン (1) 反対側から出てくる
+  x += vx;
+  y += vy;
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
   if(y > height){ y = 0; }
@@ -23,13 +27,18 @@ function draw(){
 function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
   }
   endShape(CLOSE);
+}
+
+function mouseClicked(){
+  x = mouseX;
+  y = mouseY;
 }
 
 function windowResized(){
